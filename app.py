b994270bcf445a040c206589ff1b351366451080
syoto09 <syoto09rebecca@gmail.com>
2022-05-08 19:23:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.txt == "Thank you":
-        reply_message = "NO Proble"
+        reply_message = "No problem"
     else:
         reply_message = f"You sent {event.message.txt}"
 
